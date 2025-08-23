5b64b391bca0bd5c23a6c8429750c3e52aa13db5
Юрий <avalanchedoto@mail.ru>
2025-08-24 00:53:45 +0300

    3 commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {PostComponent} from '../../shared/components/post/post.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    PostComponent,
+    NgForOf
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
