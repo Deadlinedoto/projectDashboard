baa79d0bec5280546d8cdf8fe16c9aea00d03c30
Юрий <avalanchedoto@mail.ru>
2025-09-01 02:34:13 +0300

    31.08 commit (Начал делать формы для авторизации и регистрации, сделал верстку модалки и создал форму для авторизации)

--- src/app/features/auth/components/auth/auth.component.ts
+++ src/app/features/auth/components/auth/auth.component.ts
@@ -1,10 +1,17 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Dialog} from 'primeng/dialog';
+import {Checkbox} from 'primeng/checkbox';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ButtonComponent} from '../../../../shared/components/ui/button/button.component';
 
 @Component({
   selector: 'app-auth',
   imports: [
-    Dialog
+    Dialog,
+    Checkbox,
+    FormsModule,
+    ButtonComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
@@ -13,8 +20,22 @@ export class AuthComponent {
   @Input() visible: boolean = false;
   @Output() closeShowPopupLogin = new EventEmitter();
 
+  rememberMe: boolean = false;
+
   onHide(): void {
     this.visible = false;
     this.closeShowPopupLogin.emit(this.visible);
+  }
+
+  form = new FormGroup({
+    login: new FormControl<string | null>(null, [Validators.required]),
+    password: new FormControl<string | null>(null, [Validators.required]),
+  })
+
+  onSubmit() {
+    console.log("Форма отпрвалена")
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
   }
 }
